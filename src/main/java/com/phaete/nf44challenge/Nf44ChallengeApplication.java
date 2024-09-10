882a7c6d79b87404faa339c61a66b338f46ae462
Phaete <pel4nk@gmail.com>
2024-09-10 11:01:34 +0200

    Initial commit

---
+++ src/main/java/com/phaete/nf44challenge/Nf44ChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.phaete.nf44challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nf44ChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nf44ChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/phaete/nf44challenge/Nf44ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phaete.nf44challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nf44ChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
